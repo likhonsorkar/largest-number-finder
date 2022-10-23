345f7fdbf554bf68086bd19dce996c543f113d3c
MD Likhon Sorkar <112229947+likhonsorkar@users.noreply.github.com>
2022-10-23 12:11:23 +0600

    update
    some bugs fixed

--- script.js
+++ script.js
@@ -13,15 +13,15 @@ btn.addEventListener('click', function (e) {
         if(num1 =="" || num2 =="" || num3 ==""){
             alert("please enter 3 number");
         }
-        else if(num1 > num2 && num1 > num3) {
+        else if(num1 >= num2 && num1 >= num3) {
             largest = num1;
             result.innerText = `${largest} is largest number`;
         }
-        else if (num2 > num1 && num2 > num3) {
+        else if (num2 >= num1 && num2 >= num3) {
             largest = num2;
             result.innerText = `${largest} is largest number`;
         }
-        else if(num3 > num1 && num3 > num2){
+        else if(num3 >= num1 && num3 >= num2){
             largest = num3;
             result.innerText = `${largest} is largest number`;
         }
